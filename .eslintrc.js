e689804e9031a905ad2c31378f474fed923e2470
Yosuke Ota <otameshiyo23@gmail.com>
2021-02-13 23:54:21 +0900

    Dev (#5)
    * Update schema store
    
    * 0.0.1
    
    Co-authored-by: dummy <dummy@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -104,7 +104,6 @@ module.exports = {
             parserOptions: {
                 sourceType: "module",
                 ecmaVersion: 2020,
-                parser: "babel-eslint",
             },
             globals: {
                 window: true,
