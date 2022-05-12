3c17b862c08325eeff876e34c2dffb64af472bd2
Yosuke Ota <otameshiyo23@gmail.com>
2022-05-12 21:22:49 +0900

    Use synckit (#134)
    * Use synckit
    
    * test for other os
    
    * fix for win

--- src/utils/http-client/sync-http.ts
+++ src/utils/http-client/sync-http.ts
@@ -1,6 +1,7 @@
 import type { RequestOptions } from "https"
-import { spawnSync } from "child_process"
-import path from "path"
+import { createSyncFn } from "synckit"
+
+const getSync = createSyncFn(require.resolve("./worker"))
 
 /**
  * Synchronously GET Method
@@ -10,30 +11,5 @@ export function syncGet(
     options?: RequestOptions,
     httpModulePath?: string,
 ): string {
-    const httpScriptPath = require.resolve("./cli")
-    const execPath = process.execPath
-    const optionsJSON = JSON.stringify(options ?? {})
-    const cliArgs = [
-        httpScriptPath,
-        url,
-        optionsJSON,
-        JSON.stringify({ httpModulePath }),
-    ]
-    if (path.extname(httpScriptPath) === ".ts") {
-        cliArgs.unshift("--require", "ts-node/register/transpile-only")
-    }
-    const result = spawnSync(execPath, cliArgs, {
-        windowsHide: true,
-        maxBuffer: Infinity,
-    })
-
-    if (result.error) {
-        throw result.error
-    }
-    if (result.status !== 0) {
-        throw new Error(
-            `Failed:\n${result.stdout.toString()}\n${result.stderr.toString()}`,
-        )
-    }
-    return result.stdout.toString("utf8")
+    return getSync(url, options, httpModulePath)
 }
