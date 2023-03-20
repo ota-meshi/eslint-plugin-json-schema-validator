fc44d2450fffa351f3a6d82634034e4df3ee1ddf
ota-meshi <otameshiyo23@gmail.com>
2023-03-20 17:40:57 +0900

    chore: use changesets

--- tools/update-docs.ts
+++ tools/update-docs.ts
@@ -2,6 +2,7 @@ import path from "path";
 import fs from "fs";
 import { rules } from "../src/utils/rules";
 import type { RuleModule } from "../src/types";
+import { getNewVersion } from "./lib/changesets-util";
 
 //eslint-disable-next-line require-jsdoc -- tools
 function formatItems(items: string[]) {
@@ -24,18 +25,24 @@ function yamlValue(val: unknown) {
 const ROOT = path.resolve(__dirname, "../docs/rules");
 
 //eslint-disable-next-line require-jsdoc -- tools
-function pickSince(content: string): string | null {
-  const fileIntro = /^---\n(?:.*\n)+---\n*/.exec(content);
+function pickSince(content: string): string | null | Promise<string> {
+  const fileIntro = /^---\n(?<content>.*\n)+---\n*/u.exec(content);
   if (fileIntro) {
-    const since = /since: "?(v\d+\.\d+\.\d+)"?/.exec(fileIntro[0]);
+    const since = /since: "?(?<version>v\d+\.\d+\.\d+)"?/u.exec(
+      fileIntro.groups!.content
+    );
     if (since) {
-      return since[1];
+      return since.groups!.version;
     }
   }
   // eslint-disable-next-line no-process-env -- ignore
   if (process.env.IN_VERSION_SCRIPT) {
     // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports -- ignore
     return `v${require("../package.json").version}`;
+  }
+  // eslint-disable-next-line no-process-env -- ignore
+  if (process.env.IN_VERSION_CI_SCRIPT) {
+    return getNewVersion().then((v) => `v${v}`);
   }
   return null;
 }
@@ -47,7 +54,7 @@ class DocFile {
 
   private content: string;
 
-  private readonly since: string | null;
+  private readonly since: string | null | Promise<string>;
 
   public constructor(rule: RuleModule) {
     this.rule = rule;
@@ -64,8 +71,10 @@ class DocFile {
     const {
       meta: {
         fixable,
+        hasSuggestions,
         deprecated,
-        docs: { ruleId, description, categories, replacedBy },
+        replacedBy,
+        docs: { ruleId, description, categories },
       },
     } = this.rule;
     const title = `# ${ruleId}\n\n> ${description}`;
@@ -101,9 +110,14 @@ class DocFile {
         "- :wrench: The `--fix` option on the [command line](https://eslint.org/docs/user-guide/command-line-interface#fixing-problems) can automatically fix some of the problems reported by this rule."
       );
     }
+    if (hasSuggestions) {
+      notes.push(
+        "- :bulb: Some problems reported by this rule are manually fixable by editor [suggestions](https://eslint.org/docs/developer-guide/working-with-rules#providing-suggestions)."
+      );
+    }
     if (!this.since) {
       notes.unshift(
-        `- :exclamation: <badge text="This rule has not been released yet." vertical="middle" type="error"> ***This rule has not been released yet.*** </badge>`
+        `- :exclamation: <badge text="This rule has not been released yet." vertical="middle" type="error"> **_This rule has not been released yet._** </badge>`
       );
     }
 
@@ -112,11 +126,14 @@ class DocFile {
       notes.push("", "");
     }
 
-    const headerPattern = /#.+\n+[^\n]*\n+(?:- .+\n+)*\n*/u;
+    const headerPattern = /(?:^|\n)#.+\n+[^\n]*\n+(?:- .+\n+)*\n*/u;
 
-    const header = `${title}\n\n${notes.join("\n")}`;
+    const header = `\n${title}\n\n${notes.join("\n")}`;
     if (headerPattern.test(this.content)) {
-      this.content = this.content.replace(headerPattern, header);
+      this.content = this.content.replace(
+        headerPattern,
+        header.replace(/\$/gu, "$$$$")
+      );
     } else {
       this.content = `${header}${this.content.trim()}\n`;
     }
@@ -124,15 +141,16 @@ class DocFile {
     return this;
   }
 
-  public updateFooter() {
+  public async updateFooter() {
     const { ruleName } = this.rule.meta.docs;
     const footerPattern =
-      /## (?:(?::mag:)? ?Implementation|:rocket: Version).+$/s;
+      /## (?:(?::mag:)? ?Implementation|:rocket: Version).+$/su;
     const footer = `${
       this.since
         ? `## :rocket: Version
 
-This rule was introduced in eslint-plugin-json-schema-validator ${this.since}
+This rule was introduced in eslint-plugin-json-schema-validator ${await this
+            .since}
 
 `
         : ""
@@ -143,7 +161,10 @@ This rule was introduced in eslint-plugin-json-schema-validator ${this.since}
 - [Test fixture sources](https://github.com/ota-meshi/eslint-plugin-json-schema-validator/tree/master/tests/fixtures/rules/${ruleName})
 `;
     if (footerPattern.test(this.content)) {
-      this.content = this.content.replace(footerPattern, footer);
+      this.content = this.content.replace(
+        footerPattern,
+        footer.replace(/\$/gu, "$$$$")
+      );
     } else {
       this.content = `${this.content.trim()}\n\n${footer}`;
     }
@@ -155,9 +176,9 @@ This rule was introduced in eslint-plugin-json-schema-validator ${this.since}
     const { meta } = this.rule;
 
     this.content = this.content.replace(
-      /<eslint-code-block(.*?)>/gu,
-      (_t, str) => {
-        const ps = str
+      /<eslint-code-block(?<attrs>.*?)>/gu,
+      (_t, attrs) => {
+        const ps = attrs
           .split(/\s+/u)
           .map((s: string) => s.trim())
           .filter((s: string) => s && s !== "fix");
@@ -174,8 +195,8 @@ This rule was introduced in eslint-plugin-json-schema-validator ${this.since}
   public adjustCodeBlocks() {
     // Adjust the necessary blank lines before and after the code block so that GitHub can recognize `.md`.
     this.content = this.content.replace(
-      /(<eslint-code-block[\s\S]*?>)\n+```/gu,
-      "$1\n\n```"
+      /(?<startTag><eslint-code-block[\s\S]*?>)\n+```/gu,
+      "$<startTag>\n\n```"
     );
     this.content = this.content.replace(
       /```\n+<\/eslint-code-block>/gu,
@@ -184,7 +205,7 @@ This rule was introduced in eslint-plugin-json-schema-validator ${this.since}
     return this;
   }
 
-  public updateFileIntro() {
+  public async updateFileIntro() {
     const { ruleId, description } = this.rule.meta.docs;
 
     const fileIntro = {
@@ -192,17 +213,20 @@ This rule was introduced in eslint-plugin-json-schema-validator ${this.since}
       sidebarDepth: 0,
       title: ruleId,
       description,
-      ...(this.since ? { since: this.since } : {}),
+      ...(this.since ? { since: await this.since } : {}),
     };
     const computed = `---\n${Object.keys(fileIntro)
       // eslint-disable-next-line @typescript-eslint/no-explicit-any -- tool
       .map((key) => `${key}: ${yamlValue((fileIntro as any)[key])}`)
-      .join("\n")}\n---\n`;
+      .join("\n")}\n---\n\n`;
 
     const fileIntroPattern = /^---\n(?:.*\n)+?---\n*/gu;
 
     if (fileIntroPattern.test(this.content)) {
-      this.content = this.content.replace(fileIntroPattern, computed);
+      this.content = this.content.replace(
+        fileIntroPattern,
+        computed.replace(/\$/gu, "$$$$")
+      );
     } else {
       this.content = `${computed}${this.content.trim()}\n`;
     }
@@ -211,21 +235,23 @@ This rule was introduced in eslint-plugin-json-schema-validator ${this.since}
   }
 
   public write() {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires -- tools
-    const isWin = require("os").platform().startsWith("win");
-
-    this.content = this.content.replace(/\r?\n/gu, isWin ? "\r\n" : "\n");
+    this.content = this.content.replace(/\r?\n/gu, "\n");
 
     fs.writeFileSync(this.filePath, this.content);
   }
 }
 
-for (const rule of rules) {
-  DocFile.read(rule)
-    .updateHeader()
-    .updateFooter()
-    .updateCodeBlocks()
-    .updateFileIntro()
-    .adjustCodeBlocks()
-    .write();
+void main();
+
+/** main */
+async function main() {
+  for (const rule of rules) {
+    const doc = DocFile.read(rule);
+    doc.updateHeader();
+    await doc.updateFooter();
+    doc.updateCodeBlocks();
+    await doc.updateFileIntro();
+    doc.adjustCodeBlocks();
+    doc.write();
+  }
 }
