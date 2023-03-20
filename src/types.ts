c67c75be419cb497b5739224d930f3e020fe4a54
Yosuke Ota <otameshiyo23@gmail.com>
2023-03-20 17:44:23 +0900

    chore: use changesets (#201)

--- src/types.ts
+++ src/types.ts
@@ -23,13 +23,14 @@ export interface RuleMetaData {
     url: string;
     ruleId: string;
     ruleName: string;
-    replacedBy?: string[];
     default?: "error" | "warn";
   };
   messages: { [messageId: string]: string };
   fixable?: "code" | "whitespace";
+  hasSuggestions?: boolean;
   schema: JSONSchema4 | JSONSchema4[];
   deprecated?: boolean;
+  replacedBy?: string[];
   type: "problem" | "suggestion" | "layout";
 }
 
@@ -45,13 +46,14 @@ export interface PartialRuleMetaData {
   docs: {
     description: string;
     categories: "recommended"[] | null;
-    replacedBy?: string[];
     default?: "error" | "warn";
   };
   messages: { [messageId: string]: string };
   fixable?: "code" | "whitespace";
+  hasSuggestions?: boolean;
   schema: JSONSchema4 | JSONSchema4[];
   deprecated?: boolean;
+  replacedBy?: string[];
   type: "problem" | "suggestion" | "layout";
 }
 
