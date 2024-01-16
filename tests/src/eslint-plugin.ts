35a0b2e2a95d9a0afbc5e6f5e981b68f183bca8e
ota-meshi <otameshiyo23@gmail.com>
2024-01-16 11:47:14 +0900

    fix

--- tests/src/eslint-plugin.ts
+++ tests/src/eslint-plugin.ts
@@ -1,28 +1,28 @@
 import path from "path";
 import assert from "assert";
 import plugin from "../../src/index";
-import { ESLint } from "eslint";
-import semver from "semver";
+import { getLegacyESLint } from "eslint-compat-utils/eslint";
 
+// eslint-disable-next-line @typescript-eslint/naming-convention -- Class name
+const ESLint = getLegacyESLint();
 // -----------------------------------------------------------------------------
 // Tests
 // -----------------------------------------------------------------------------
 
 const TEST_CWD = path.join(__dirname, "../fixtures/integrations/eslint-plugin");
 
-if (semver.lt(ESLint.version, "9.0.0-0"))
-  describe("Integration with eslint-plugin-json-schema-validator", () => {
-    it("should lint without errors", async () => {
-      const engine = new ESLint({
-        cwd: TEST_CWD,
-        extensions: [".js", ".json"],
-        plugins: { "json-schema-validator": plugin as any },
-      });
-      const results = await engine.lintFiles(["test01/src"]);
-      assert.strictEqual(results.length, 2);
-      assert.strictEqual(
-        results.reduce((s, r) => s + r.errorCount, 0),
-        0,
-      );
+describe("Integration with eslint-plugin-json-schema-validator", () => {
+  it("should lint without errors", async () => {
+    const engine = new ESLint({
+      cwd: TEST_CWD,
+      extensions: [".js", ".json"],
+      plugins: { "json-schema-validator": plugin as any },
     });
+    const results = await engine.lintFiles(["test01/src"]);
+    assert.strictEqual(results.length, 2);
+    assert.strictEqual(
+      results.reduce((s, r) => s + r.errorCount, 0),
+      0,
+    );
   });
+});
