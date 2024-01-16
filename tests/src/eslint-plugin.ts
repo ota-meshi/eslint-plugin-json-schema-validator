bd2b11312011bdb5ebf0cb31709541faf2fc5c6e
ota-meshi <otameshiyo23@gmail.com>
2024-01-16 11:18:55 +0900

    fix

--- tests/src/eslint-plugin.ts
+++ tests/src/eslint-plugin.ts
@@ -1,11 +1,8 @@
 import path from "path";
 import assert from "assert";
 import plugin from "../../src/index";
-import { Linter } from "eslint";
+import { ESLint } from "eslint";
 import semver from "semver";
-import { getESLint } from "eslint-compat-utils/eslint";
-// eslint-disable-next-line @typescript-eslint/naming-convention -- Class name
-const ESLint = getESLint();
 
 // -----------------------------------------------------------------------------
 // Tests
@@ -13,16 +10,13 @@ const ESLint = getESLint();
 
 const TEST_CWD = path.join(__dirname, "../fixtures/integrations/eslint-plugin");
 
-if (semver.lt(Linter.version, "9.0.0-0"))
+if (semver.lt(ESLint.version, "9.0.0-0"))
   describe("Integration with eslint-plugin-json-schema-validator", () => {
     it("should lint without errors", async () => {
       const engine = new ESLint({
         cwd: TEST_CWD,
         extensions: [".js", ".json"],
-        overrideConfig: {
-          files: ["**/*.js", "**/*.json", "*.js", "*.json"],
-          plugins: { "json-schema-validator": plugin },
-        } as any,
+        plugins: { "json-schema-validator": plugin as any },
       });
       const results = await engine.lintFiles(["test01/src"]);
       assert.strictEqual(results.length, 2);
