614821de9e3b6d71967e3e26cc50ea0d80b4c49b
yosuke ota <otameshiyo23@gmail.com>
2026-01-22 21:27:59 +0900

    fix: update ext calculation to use fileURLToPath for import.meta.url

--- src/utils/http-client/sync-http.ts
+++ src/utils/http-client/sync-http.ts
@@ -2,9 +2,10 @@ import type { RequestOptions } from "https";
 import { createRequire } from "module";
 import path from "path";
 import { createSyncFn } from "synckit";
+import { fileURLToPath } from "url";
 
 const require = createRequire(import.meta.url);
-const ext = path.extname(import.meta.filename);
+const ext = path.extname(fileURLToPath(import.meta.url));
 const getSync = createSyncFn(require.resolve(`./worker${ext}`));
 
 /**
